2b9d808e1b5da41e43a8bb11ac6db776189f4352
Jeremy List <quick.dudley@gmail.com>
2017-09-28 17:16:10 +1300

    Simple sequence data structure.

--- src/AI/HFNN/Internal.hs
+++ src/AI/HFNN/Internal.hs
@@ -3,8 +3,9 @@ module AI.HFNN.Internal (
 
  ) where
 
+import Data.Semigroup
 import Foreign.Ptr
-import Foreign.ForeignPtr
+import Data.Array.Storable
 import Foreign.Storable
 import System.IO.Unsafe
 
@@ -18,3 +19,45 @@ data WeightSelector = WeightSelector {
   getWeight :: Ptr Double -> Int -> Int -> IO Double,
   updateWeight :: Ptr Double -> Int -> Int -> (Double -> Double) -> IO ()
  }
+
+-- Quick and dirty tree list. Won't bother balancing because we only need
+-- to build and traverse: no need to lookup by index.
+data CatTree a = Run !Int a | CatNode !Int (CatTree a) (CatTree a)
+
+catTreeSize (Run s _) = s
+catTreeSize (CatNode s _ _) = s
+
+instance Semigroup (CatTree a) where
+  a <> b = CatNode (catTreeSize a + catTreeSize b) a b
+
+instance Functor CatTree where
+  fmap f = go where
+    go (Run l a) = Run l (f a)
+    go (CatNode s a b) = CatNode s (go a) (go b)
+
+instance Foldable CatTree where
+  foldMap f = go where
+    go (Run l a) = mr l where
+      t = f a
+      mr 1 = t
+      mr n = let
+        (nl,m) = n `divMod` 2
+        nr = nl + m
+        in mappend (mr nl) (mr nr)
+    go (CatNode _ a b) = mappend (go a) (go b)
+
+-- Cannot implement "instance Traversable CatTree" without sacrificing
+-- run-length compression.
+
+instance Applicative CatTree where
+  pure = Run 1
+  (Run lf f) <*> a' = go a' where
+    go (Run lr a) = Run (lf * lr) (f a)
+    go (CatNode _ a b) = go a <> go b
+  (CatNode _ a b) <*> r = (a <*> r) <> (b <*> r)
+
+instance Monad CatTree where
+  return = pure
+  (Run l a) >>= f = go (f a) where
+    go (Run l' b) = Run (l' * l) b
+    go (CatNode _ x y) = go x <> go y
