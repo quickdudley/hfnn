3e14156526328169480bcc6354aa7bccdfa61f38
Jeremy List <quick.dudley@gmail.com>
2017-10-01 11:29:14 +1300

    Catch potential bugs in advance

--- src/AI/HFNN/Internal.hs
+++ src/AI/HFNN/Internal.hs
@@ -4,6 +4,7 @@ module AI.HFNN.Internal (
  ) where
 
 import Data.Semigroup
+import Data.Word
 import Foreign.Ptr
 import Data.Array.Storable
 import Foreign.Storable
@@ -22,18 +23,29 @@ data WeightSelector = WeightSelector {
 
 -- Quick and dirty tree list. Won't bother balancing because we only need
 -- to build and traverse: no need to lookup by index.
-data CatTree a = Run !Int a | CatNode !Int (CatTree a) (CatTree a)
+data CatTree a =
+  Run !Word a |
+  CatNode !Word (CatTree a) (CatTree a) |
+  CatNil
 
 catTreeSize (Run s _) = s
 catTreeSize (CatNode s _ _) = s
+catTreeSize CatNil = 0
 
 instance Semigroup (CatTree a) where
+  CatNil <> b = b
+  a <> CatNil = a
   a <> b = CatNode (catTreeSize a + catTreeSize b) a b
+
+instance Monoid (CatTree a) where
+  mappend = (<>)
+  mempty = CatNil
 
 instance Functor CatTree where
   fmap f = go where
     go (Run l a) = Run l (f a)
     go (CatNode s a b) = CatNode s (go a) (go b)
+    go CatNil = CatNil
 
 instance Foldable CatTree where
   foldMap f = go where
@@ -45,23 +57,68 @@ instance Foldable CatTree where
         nr = nl + m
         in mappend (mr nl) (mr nr)
     go (CatNode _ a b) = mappend (go a) (go b)
+    go CatNil = mempty
 
--- Cannot implement "instance Traversable CatTree" without sacrificing
--- run-length compression.
+instance Traversable CatTree where
+  sequenceA CatNil = pure CatNil
+  sequenceA (CatNode s a b) = CatNode s <$> sequenceA a <*> sequenceA b
+  sequenceA r@(Run 1 a) = Run 1 <$> a
+  sequenceA r@(Run _ _) = sequenceA $ expandCT r
+
+expandCT :: CatTree a -> CatTree a
+expandCT (Run s0 a) = let
+  e 0 = CatNil
+  e 1 = Run 1 a
+  e n = let
+    n' = n `div` 2
+    in CatNode n (e n') (e (n - n'))
+  in e s0
+expandCT (CatNode s a b) = CatNode s (expandCT a) (expandCT b)
+expandCT CatNil = CatNil
+
+splitCT :: CatTree a -> Word -> (CatTree a, CatTree a)
+splitCT CatNil _ = (CatNil, CatNil)
+splitCT r 0 = (CatNil, r)
+splitCT r@(Run n a) s = if n <= s
+  then (r,CatNil)
+  else (Run s a, Run (s - n) a)
+splitCT r@(CatNode n a b) s = if n <= s
+  then (r,CatNil)
+  else case catTreeSize a `compare` s of
+    EQ -> (a, b)
+    LT -> let
+      (l,b') = splitCT b (s - catTreeSize a)
+      in (a <> l, b')
+    GT -> let
+      (a',l) = splitCT a s
+      in (a', l <> b)
 
 instance Applicative CatTree where
   pure = Run 1
-  (Run lf f) <*> a' = go a' where
-    go (Run lr a) = Run (lf * lr) (f a)
+  CatNil <*> _ = CatNil
+  _ <*> CatNil = CatNil
+  r@(Run lf f) <*> a' = let
+    rt = fmap f a'
     go (CatNode _ a b) = go a <> go b
+    go (Run 1 _) = rt
+    go r@(Run _ _) = go (expandCT r)
+    go CatNil = CatNil
+    in go r
   (CatNode _ a b) <*> r = (a <*> r) <> (b <*> r)
 
 instance Monad CatTree where
   return = pure
-  (Run l a) >>= f = go (f a) where
-    go (Run l' b) = Run (l' * l) b
-    go (CatNode _ x y) = go x <> go y
+  fail = const CatNil
+  r@(Run _ a) >>= f = let
+    rt = f a
+    go (CatNode _ a b) = go a <> go b
+    go (Run 1 _) = rt
+    go r@(Run _ _) = go (expandCT r)
+    go CatNil = CatNil
+    in go r
+  (CatNode _ a b) >>= f = (a >>= f) <> (b >>= f)
+  CatNil >>= _ = CatNil
 
-reverseCT :: CatTree -> CatTree
+reverseCT :: CatTree a -> CatTree a
 reverseCT r@(Run _ _) = r
 reverseCT (CatNode l a b) = CatNode l (reverseCT b) (reverseCT a)
