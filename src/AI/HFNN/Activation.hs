288bbd1f76a18c03682b9acedf66bbfb23045f92
Jeremy List <quick.dudley@gmail.com>
2017-10-03 20:57:51 +1300

    Add softmax (slightly wrong, should still converge properly)

--- src/AI/HFNN/Activation.hs
+++ src/AI/HFNN/Activation.hs
@@ -33,23 +33,33 @@ modifiedCuberoot = nogl {
     in (signum x' * (
       crt - (atan (sr3 * crt))/sr3
      ), 1 / (3 * x**(2/3) + 1))
-}
+ }
 
 logistic :: ActivationFunction
 logistic = nogl {
   activationFunction = map $ \x -> let
     y = 1 / (1 + exp (-x))
     in (y, y * (1 - y))
-}
+ }
 
 relu :: ActivationFunction 
 relu = nogl {
   activationFunction = map $ \x -> if x < 0
     then (0, 0)
     else (x, 1)
-}
+ }
 
 softplus :: ActivationFunction
 softplus = nogl {
   activationFunction = map $ \x -> (log (1 + exp x), 1 / (1 + exp (-x)))
-}
+ }
+
+softmax :: ActivationFunction
+softmax = nogl {
+  activationFunction = \z -> let
+    ez = map exp z
+    d = sum ez
+    in map (\q -> let
+      s = q / d
+      in (s, s * (1 - s))) ez
+ }
