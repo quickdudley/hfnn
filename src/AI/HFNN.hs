5ff2d7f0d85dc8100801ee0deb9592a30f75ebfe
Jeremy List <quick.dudley@gmail.com>
2017-10-30 12:46:16 +1300

    Added two convenience functions

--- src/AI/HFNN.hs
+++ src/AI/HFNN.hs
@@ -1,5 +1,29 @@
+{-# LANGUAGE DataKinds #-}
 module AI.HFNN (
-
+  module AI.HFNN.Internal,
+  module AI.HFNN.Activation,
+  simpleLayer,
+  backpropExample
  ) where
 
+import Control.Applicative
+import Control.Monad
+import AI.HFNN.Activation
 import AI.HFNN.Internal
+
+simpleLayer :: [Layer s] -> Word -> ActivationFunction ->
+  NNBuilder d s (Layer s)
+simpleLayer l' s af = do
+  let l = bias : l
+  w <- forM l $ \l1 -> addBaseWeights (layerSize l1) s
+  Just nl <- standardLayer (zip l w) af
+  return nl
+
+backpropExample :: FeedForward d -> [Maybe Double] ->
+  (WeightUpdate, InputTension)
+backpropExample ff ex = let
+  er = zipWith (\e o -> case e of
+    Just e' -> e' - o
+    _ -> 0
+   ) ex (getOutputs ff)
+  in backPropagate ff er
